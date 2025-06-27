a15ca1ce56449c8b937f6837fa21c225f7c4a41f
omzkiii <geogeomarsantos@gmail.com>
2025-06-27 14:10:04 +0800

    add check line logic

--- post.sh
+++ post.sh
@@ -1,4 +1,13 @@
-curl -H 'Content-Type: application/json' \
-    -d '{"coor": ['$1','$2']}' \
-    -X POST \
-    localhost:3000/click
+post() {
+    curl -H 'Content-Type: application/json' \
+        -d '{"coor": ['$1','$2']}' \
+        -X POST \
+        localhost:3000/click
+
+}
+
+post 0 0
+post 0 2
+post 1 1
+post 2 0
+post 2 2
--- server.ts
+++ server.ts
@@ -6,7 +6,7 @@ const board = [
   [0, 0, 0],
 ];
 
-function checkMatch(p: number[], m: number[][]): boolean {
+function match(p: number[], m: number[][]): boolean {
   return m.some((el) => {
     return el.every((val, idx) => {
       console.log("val: " + val);
@@ -16,15 +16,45 @@ function checkMatch(p: number[], m: number[][]): boolean {
   });
 }
 
+let p1: number[][] = [];
+let p2: number[][] = [];
+const LINES1 = { vert: 0, hor: 0, diag: 0 };
+const LINES2 = { vert: 0, hor: 0, diag: 0 };
+
+function check(m: number[][], LINES) {
+  if (m.length < 2) {
+    return;
+  }
+
+  for (let i = 0; i <= m.length - 1; i++) {
+    const [x1, y1] = m[i];
+    const [x2, y2] = m[m.length - 1];
+    const slope = Math.abs((y2 - y1) / (x2 - x1));
+    console.log(`Slope of ${m[i]} and ${m[m.length - 1]}: ${slope}`);
+    switch (slope) {
+      case Infinity | NaN:
+        LINES.vert++;
+        break;
+      case 0:
+        LINES.hor++;
+        break;
+      case 1:
+        LINES.diag++;
+        break;
+      default:
+        break;
+    }
+  }
+  console.log("STATS: " + JSON.stringify(LINES));
+}
+
 let player = -1;
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send(board);
+  res.send(Math.abs(0 / 1).toString());
 });
 
-let p1: number[][] = [];
-let p2: number[][] = [];
 app.post("/click", (req: Request, res: Response): void => {
   const coor = req.body.coor;
   if (coor.some((val: number) => val < 0 || val > 3)) {
@@ -32,7 +62,7 @@ app.post("/click", (req: Request, res: Response): void => {
     res.send(board + "\n INVALID MOVE\n");
     return;
   }
-  if (checkMatch(coor, p1) || checkMatch(coor, p2)) {
+  if (match(coor, p1) || match(coor, p2)) {
     console.log("INVALID MOVE");
     res.send(board + "\n INVALID MOVE\n");
     return;
@@ -41,8 +71,12 @@ app.post("/click", (req: Request, res: Response): void => {
   player = player * -1;
   if (player === 1) {
     p1.push(coor);
+    console.log("P1: " + p1);
+    check(p1, LINES1);
   } else if (player === -1) {
     p2.push(coor);
+    console.log("P2: " + p2);
+    check(p2, LINES2);
   }
   board[coor[0]][coor[1]] = player;
   console.log("VALID MOVE");
