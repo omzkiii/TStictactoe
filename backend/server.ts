0553f6c7fd7fdcb7ab3b45e7487b21c5c60ca9ea
omzkiii <geogeomarsantos@gmail.com>
2025-06-30 20:44:19 +0800

    add tsc confgfile

--- backend/server.ts
+++ backend/server.ts
@@ -1,19 +1,38 @@
 import express, { Request, Response } from "express";
 import { createClient } from "redis";
+import { randomUUID } from "crypto";
 import session from "express-session";
 
 const client = createClient({ url: "redis://valkey:6379" });
 client.on("error", (err) => console.log(err));
 client.connect();
 const app = express();
+
 app.use(express.json());
+app.use(
+  session({
+    secret: "key",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      path: "/",
+      maxAge: 15000,
+      secure: false,
+    },
+  }),
+);
 
 app.get("/", async (req: Request, res: Response) => {
-  res.send("OK");
+  req.session.visited = true;
+  res.cookie;
+  console.log(req.sessionID);
+  console.log(req.session.visited);
+  res.send(req.headers.cookie?.toString());
 });
 app.get("/get", async (req: Request, res: Response) => {
-  const value = await client.get("player");
-  res.send(value);
+  console.log(req.sessionID);
+  console.log(req.session.visited);
+  res.send(req.headers.cookie?.toString());
 });
 
 app.post("/click", (req: Request, res: Response): void => {});
