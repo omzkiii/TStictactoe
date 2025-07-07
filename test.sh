44c387e53f6086aba3e90241cdbc7f970a6f8eac
omzkiii <geogeomarsantos@gmail.com>
2025-07-07 22:55:09 +0800

    fix curl script

--- test.sh
+++ test.sh
@@ -1,2 +1,16 @@
+# curl -c cookie.txt -b cookie.txt -d '{"coor": [2, 2], "player": 1}' -H 'Content-Type: application/json' localhost:3000/move
+
 curl -c cookie.txt -b cookie.txt localhost:3000
-curl -c cookie.txt -b cookie.txt -d '{"coor": [2, 2], "player": 1}' -H 'Content-Type: application/json' localhost:3000/move
+post() {
+    curl -c cookie.txt -b cookie.txt \
+        -d '{"coor": ['$1', '$2']}' \
+        -X POST \
+        -H 'Content-Type: application/json' \
+        localhost:3000/move
+}
+
+post 0 0
+post 0 2
+post 1 1
+post 2 0
+post 2 2
