0762dfad53a70b43e7b10ae862b7856d947d2f23
omzkiii <geogeomarsantos@gmail.com>
2025-07-08 20:46:46 +0800

    testing vue directive

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -4,14 +4,16 @@ import { ref } from 'vue'
 const count = ref(0)
 const foo = "text"
 const bar = "text2"
+const dosomething = () => {
+  count.value = 10
+  console.log(count)
+}
 
 </script>
 
 <template>
   <button @click="count++">Count is: {{ count }}</button>
-  <div :id="bar">HELLO WORLD</div>
-
-
+  <div v-on:click="dosomething">{{ count }}</div>
 </template>
 
 <style scoped>
