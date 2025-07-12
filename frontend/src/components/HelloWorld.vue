0d8e6665b294f7f20f265621f7a4376f32a43b21
omzkiii <geogeomarsantos@gmail.com>
2025-07-13 04:42:50 +0800

    add alternating 'X' and 'O' button using computed and ref in frontend

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,21 +1,35 @@
 <script setup lang="ts">
+import { computed } from '@vue/reactivity';
 import { ref } from 'vue';
 // import useCounter from '../composables/App.ts';
 // const { count, increment } = useCounter()
 // import axios from 'axios';
 
-const bLabel = ref(' ')
+const bLabel = ref(1)
 
+const move = computed(() => {
+  switch (bLabel.value) {
+    case 1:
+      return 'X'
+      break;
+    case -1:
+      return 'O'
+      break;
+    default:
+      break;
+  }
+}
+)
 
 function greet() {
-  bLabel.value = 'X'
+  bLabel.value = bLabel.value * -1
 }
 
 </script>
 
 <template>
   <button @click="greet">
-    {{ bLabel }}
+    {{ move }}
   </button>
 </template>
 
