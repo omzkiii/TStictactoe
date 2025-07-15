460104df26a794fd82b999d73c891d982b2c272c
omzkiii <geogeomarsantos@gmail.com>
2025-07-15 22:55:40 +0800

    add Square.vue

---
+++ frontend/src/components/Square.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import axios from 'axios';
+const props = defineProps(['coor']);
+const url = import.meta.env.VITE_API_BASE_URL
+
+const move = () => {
+  axios.get(`${url}/get`)
+}
+
+
+</script>
+
+<template>
+  <button @click="move">CLICK</button>
+
+</template>
+
+<style scoped>
+button {
+  background-color: #FFF;
+  width: 5em;
+  height: 5em;
+  /* padding: 0; */
+  /* border: none; */
+}
+</style>
