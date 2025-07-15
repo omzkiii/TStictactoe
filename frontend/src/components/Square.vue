f426510a28c254d75a7051335b2b4aaa6306f358
omzkiii <geogeomarsantos@gmail.com>
2025-07-16 00:03:32 +0800

    add test api fetch in square.vue

--- frontend/src/components/Square.vue
+++ frontend/src/components/Square.vue
@@ -3,25 +3,25 @@ import { ref } from 'vue';
 import axios from 'axios';
 const props = defineProps(['coor']);
 const url = import.meta.env.VITE_API_BASE_URL
+const data = ref('')
 
-const move = () => {
-  axios.get(`${url}/get`)
+async function test() {
+  data.value = await axios.get(`${url}/get`, { withCredentials: true }).then((res) => {
+    return res.data
+  }).catch((err) => console.log("SOMETHING WENT WRONG: " + err))
 }
-
-
 </script>
 
 <template>
-  <button @click="move">CLICK</button>
-
+  <button @click="test">{{ props.coor.x }},{{ props.coor.y }}</button>
+  <p>{{ data }}</p>
 </template>
 
 <style scoped>
 button {
+  color: #000;
   background-color: #FFF;
   width: 5em;
   height: 5em;
-  /* padding: 0; */
-  /* border: none; */
 }
 </style>
