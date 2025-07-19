1d7e778aa29a798df2e584fccaa911f27be0b5be
omzkiii <geogeomarsantos@gmail.com>
2025-07-19 20:32:39 +0800

    add move_no variable to track moves

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import Square from './components/Square.vue';
+const moves_no = ref(0)
 </script>
 
 <template>
   <div v-for="x in 3">
     <div v-for="y in 3">
-      <Square :coor="{ x: x, y: y }" />
+      <Square :coor="{ x: x, y: y }" :moves_no="moves_no" @make_move="moves_no++" />
     </div>
   </div>
   <!-- <Square /> -->
