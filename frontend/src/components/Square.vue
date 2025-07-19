dd6f50467f895151e7fb7937c97bc8cd223889a4
omzkiii <geogeomarsantos@gmail.com>
2025-07-20 02:11:58 +0800

    clean square.vue

--- frontend/src/components/Square.vue
+++ frontend/src/components/Square.vue
@@ -5,9 +5,7 @@ import { ref } from 'vue';
 const emit = defineEmits(['make_move'])
 const props = defineProps(['coor', 'moves_no']);
 // const url = import.meta.env.VITE_API_BASE_URL
-const data = ref('')
 const sign = ref('')
-
 
 function get_sign() {
   if (props.moves_no % 2 == 0) {
@@ -21,13 +19,11 @@ function get_sign() {
 function make_move() {
   get_sign()
   emit('make_move')
-
 }
 </script>
 
 <template>
   <button @click.once="make_move">{{ sign }}</button>
-  <p>{{ data }}</p>
 </template>
 
 <style scoped>
