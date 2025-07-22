39d7be269be1cb4cd8d317d918a4d74bfa49b942
omzkiii <geogeomarsantos@gmail.com>
2025-07-23 00:40:37 +0800

    frontend: use /move api to send_request

--- frontend/src/components/Square.vue
+++ frontend/src/components/Square.vue
@@ -3,8 +3,9 @@ import axios from 'axios';
 import { ref } from 'vue';
 const emit = defineEmits(['make_move'])
 const props = defineProps(['coor', 'moves_no']);
-const url = import.meta.env.VITE_API_BASE_URL
-const sign = ref('')
+const url = import.meta.env.VITE_API_BASE_URL;
+const sign = ref('');
+const test_data = ref();
 
 function get_sign() {
   if (props.moves_no % 2 == 0) {
@@ -13,30 +14,35 @@ function get_sign() {
   else {
     sign.value = 'O'
   }
-}
+};
 
-function send_request() {
-  const ret_val = axios.post(url + "move", {
+async function send_request() {
+  const ret_val = await axios.post(url + "/move", {
     coor: props.coor,
   }, {
     withCredentials: true
   })
-}
+  test_data.value = ret_val.data
+  // return ret_val
+};
 
 function make_move() {
   get_sign()
   emit('make_move')
-}
+  send_request()
+};
 </script>
 
 <template>
   <button @click.once="make_move">{{ sign }}</button>
+  <p>{{ test_data }}</p>
 </template>
 
 <style scoped>
 button {
   color: #000;
   background-color: #FFF;
+  border: 3px solid #000;
   width: 5em;
   height: 5em;
 }
