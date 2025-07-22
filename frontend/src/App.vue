7cf895f2380fd32691040686395cebadbb6270fa
omzkiii <geogeomarsantos@gmail.com>
2025-07-23 00:41:07 +0800

    frontend: change props from object to array

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,15 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import Square from './components/Square.vue';
-const moves_no = ref(0)
+const moves_no = ref(0);
 </script>
 
 <template>
   <div id="x" v-for="x in 3">
     <div id="y" v-for="y in 3">
-      <Square :coor="{ coor: [x, y] }" :moves_no="moves_no" @make_move="moves_no++" />
+      <Square :coor="[x, y]" :moves_no="moves_no" @make_move="moves_no++" />
     </div>
   </div>
-  <!-- <Square /> -->
 </template>
 
 <style scoped>
