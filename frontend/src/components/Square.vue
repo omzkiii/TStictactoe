2cb52a6ba3642956dadcbf46a22d42719798c5d7
omzkiii <geogeomarsantos@gmail.com>
2025-07-27 20:59:35 +0800

    frontend: add alert for game results

--- frontend/src/components/Square.vue
+++ frontend/src/components/Square.vue
@@ -16,6 +16,32 @@ function get_sign() {
   }
 };
 
+function result_handler(ret_val: number) {
+  const PLAYER1_WIN = 1
+  const PLAYER2_WIN = 2
+  const DRAW = -1
+
+  switch (ret_val) {
+    case PLAYER1_WIN:
+      alert("PLAYER 1 WIN")
+      window.location.reload()
+      break;
+
+    case PLAYER2_WIN:
+      alert("PLAYER 2 WIN")
+      window.location.reload()
+      break;
+
+    case DRAW:
+      alert("DRAW")
+      window.location.reload()
+      break;
+
+    default:
+      break;
+  }
+}
+
 async function send_request() {
   const ret_val = await axios.post(url + "/move", {
     coor: props.coor,
@@ -23,6 +49,7 @@ async function send_request() {
     withCredentials: true
   })
   test_data.value = ret_val.data
+  result_handler(ret_val.data)
 };
 
 function make_move() {
