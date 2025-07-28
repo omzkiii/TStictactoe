d686eca74583c1803ed0856a84dad53f4dbc0fbd
omzkiii <geogeomarsantos@gmail.com>
2025-07-28 20:29:37 +0800

    frontend: test /game route from backend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,23 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import Square from './components/Square.vue';
+const url = import.meta.env.VITE_API_BASE_URL;
+import axios from 'axios';
 const moves_no = ref(0);
+const game_data = ref();
+
+async function get_game_data() {
+  game_data.value = await axios.get(url + "/game", {
+    withCredentials: true
+  }).then((res) => { return res.data })
+}
+
+
 </script>
 
 <template>
+  <button @click="get_game_data"> GET DATA</button>
+  <p>{{ game_data }}</p>
   <div id="x" v-for="x in 3">
     <div id="y" v-for="y in 3">
       <Square :coor="[x, y]" :moves_no="moves_no" @make_move="moves_no++" />
