deeb04bb7796b6bbf31c2a1ad27536705dfe66ed
omzkiii <geogeomarsantos@gmail.com>
2025-07-30 00:15:17 +0800

    frontend: add button to test game data

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,15 +7,20 @@ const moves_no = ref(0);
 const game_data = ref();
 
 onMounted(async () => {
+  get_game_data()
+})
+
+async function get_game_data() {
   game_data.value = await axios.get(url + "/game", {
     withCredentials: true
   }).then((res) => { return res.data })
-})
+}
 
 </script>
 
 <template>
   <p>{{ game_data }}</p>
+  <button @click="get_game_data">GET DATA</button>
   <div id="x" v-for="x in 3">
     <div id="y" v-for="y in 3">
       <Square :coor="[x, y]" :moves_no="moves_no" @make_move="moves_no++" />
